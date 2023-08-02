665ea4d961db44f5e196624139fa94edd51aa0f1
Enc0dedClass <75113441+Enc0dedClass@users.noreply.github.com>
2023-08-02 15:56:03 +0200

    fixed a bug where it would falsly add non draw move to buffer

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -73,7 +73,6 @@ public class MyBot : IChessBot
                     }
                     else printErrorDraw(move);
                 }
-                else printErrorDraw(move);
             }
 
             board.UndoMove(move);
