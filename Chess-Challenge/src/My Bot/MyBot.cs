d8997b8ca9f94f999b833d4fafa0ef6919ed441c
Enc0dedClass <75113441+Enc0dedClass@users.noreply.github.com>
2023-08-04 09:01:20 +0200

    Update MyBot.cs

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,12 +1,13 @@
 ﻿using ChessChallenge.API;
 using System;
+using static System.Math;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 
 public class MyBot : IChessBot
 {
-    // right now funktions are seperated. before submision, everything will be compacted into the think function.
+    // right now funktions are seperated. before submision, everything will be compacted into the think function if possible.
 
     //example code
     //public Move Think(Board board, Timer timer)
@@ -14,6 +15,10 @@ public class MyBot : IChessBot
     //    Move[] moves = board.GetLegalMoves();
     //    return moves[0];
     //}
+
+    //---this section is variables designated to zobrist hashing and the transportition table---
+    byte[] boardHash = new byte[8];
+    //---end---
 
     // how much each piece is worth
     float totalPieceValue;
@@ -59,6 +64,12 @@ public class MyBot : IChessBot
     } //#DEBUG
     public Move Think(Board board, Timer timer)
     {
+        Console.WriteLine(smallRandomNumberGenerator(1)); //#DEBUG
+        Console.WriteLine(smallRandomNumberGenerator(2)); //#DEBUG
+        Console.WriteLine(smallRandomNumberGenerator(3)); //#DEBUG
+        Console.WriteLine(smallRandomNumberGenerator(4)); //#DEBUG
+        Console.WriteLine(smallRandomNumberGenerator()); //#DEBUG
+        Console.WriteLine(smallRandomNumberGenerator()); //#DEBUG
         pieceSqareValues = toPieceArray(new[] { 1010101018181818, 1212141611111215, 1010101411090810, 1112120610101010, 0002040402061010, 0410121304111314, 0410131404111213, 0206101100020404, 0608080808101010, 0810111208111112, 0810121208121212, 0811101006080808, 1010101011121212, 0910101009101010, 0910101009101010, 0910101010101011, 0608080908101010, 0810111109101111, 1010111108111111, 0810111006080809, 0402020004020200, 0402020004020200, 0604040208060606, 1414060630341207,
                                                 1010101036303230, 2015181412121413, 1212121211111111, 0909090910101010, 1010101036303230, 2015181412121413, 1212121211111111, 0909090910101010, 1010101036303230, 2015181412121413, 1212121211111111, 0909090910101010, 1010101036303230, 2015181412121413, 1212121211111111, 0909090910101010, 1010101036303230, 2015181412121413, 1212121211111111, 0909090910101010, 1010101036303230, 2015181412121413, 1212121211111111, 0909090910101010, }); // use https://onlinestringtools.com/split-string to split into 16 long parts
         //Botton is endgame
@@ -266,7 +277,7 @@ public class MyBot : IChessBot
     int smallRandomNumberGenerator(int seed = -1)
     {
         if (seed == -1) seed = prevSeed;
-        prevSeed = (int)(((((seed * 32 + 16) / 31 + 2) * 1.5f)+(seed*1.93f)-2/seed)/2.3*(seed/45+1));
+        prevSeed = Abs(((int)(((((seed * 325 + 162) / 313 + 26) * 17.5f)+(seed*17.93f)-23/(seed))/226.3*(seed/4511+15)/(seed/(4-seed/seed-2.3f)))));
         return prevSeed;
     }
 }
