cbf1d955010c2b38be4e3162e407c54f67dc7031
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-08-05 12:06:52 +0200

    renamed bestmove to bestmoves
    to make it clearer that it has several moves inside of it
    this also makes it more understandable why we have to use "[bestMoves.Length - 1]"

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -70,8 +70,8 @@ public class MyBot : IChessBot
 
         weAreWhite = board.IsWhiteToMove;
         Console.WriteLine(" ------ calculate new move -----" + board.IsWhiteToMove); //#DEBUG
-        var bestMove = miniMax(board, timer.MillisecondsRemaining < 20000 ? timer.MillisecondsRemaining < 5000 ? 2 : 3 : 4, weAreWhite ? 1 : -1, minFloatValue, float.MaxValue).Item1;
-        bestMove.ToList().ForEach(move => { Console.WriteLine(move); });
+        var bestMoves = miniMax(board, timer.MillisecondsRemaining < 20000 ? timer.MillisecondsRemaining < 5000 ? 2 : 3 : 4, weAreWhite ? 1 : -1, minFloatValue, float.MaxValue).Item1;
+        bestMoves.ToList().ForEach(move => { Console.WriteLine(move); });
         if (IsEndgame(board)){
             IsEndgameNoFunction = true;
             Console.WriteLine("We are in the endgame"); //#DEBUG
@@ -83,7 +83,7 @@ public class MyBot : IChessBot
         foundDublicateDrawMoves = 0; //#DEBUG
         Console.WriteLine("found these draw moves: "+foundDrawMoves+" this turn"); //#DEBUG
         Console.WriteLine(searchedMoves + " Searched moves"); //#DEBUG
-        return bestMove[bestMove.Length - 1];
+        return bestMoves[bestMoves.Length - 1];
         //Console.WriteLine(isPieceProtectedAfterMove(board, moves[0]));
         
     }
