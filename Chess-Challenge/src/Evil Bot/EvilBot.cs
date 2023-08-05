2f8ebe0d46b95444276a0cf0a6a61aa42b1174bb
Elliott <85990359+Ell1ott@users.noreply.github.com>
2023-08-05 13:09:49 +0200

    removed prints from evilbot

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -26,12 +26,12 @@ public class EvilBot : IChessBot
         
         pieceSqareValues = toPieceArray(new[] { 1010101018181818, 1212141611111215, 1010101411090810, 1112120610101010, 0002040402061010, 0410121304111314, 0410131404111213, 0206101100020404, 0608080808101010, 0810111208111112, 0810121208121212, 0811101006080808, 1010101011121212, 0910101009101010, 0910101009101010, 0910101010101011, 0608080908101010, 0810111109101111, 1010111108111111, 0810111006080809, 0402020004020200, 0402020004020200, 0604040208060606, 1414101014161210 }); // use https://onlinestringtools.com/split-string to split into 16 long parts
         //arrCenterDistanceInt = toPieceArray(arrCenterDistance);
-        Console.WriteLine(pieceSqareValues.Length);
-        Console.WriteLine(getPieceValue(PieceType.King, 7, 7));
+        //Console.WriteLine(pieceSqareValues.Length);
+        //Console.WriteLine(getPieceValue(PieceType.King, 7, 7));
 
 
         weAreWhite = board.IsWhiteToMove;
-        Console.WriteLine(" ------ calculate new move -----", timer);
+        //Console.WriteLine(" ------ calculate new move -----", timer);
         Move[] moves = board.GetLegalMoves();
         float bMoveMat = float.MinValue;
         foreach (var move in moves)
@@ -53,7 +53,7 @@ public class EvilBot : IChessBot
             {
                 bMove = move; 
                 bMoveMat = v;
-                Console.WriteLine("this move was better so is changing to " + move);
+                //Console.WriteLine("this move was better so is changing to " + move);
             }
             board.UndoMove(move);
 
@@ -62,7 +62,7 @@ public class EvilBot : IChessBot
         {
             return bMove;
         }
-        Console.WriteLine(isPieceProtectedAfterMove(board, moves[0]));
+        //Console.WriteLine(isPieceProtectedAfterMove(board, moves[0]));
         return moves[0];
     }
 
@@ -99,7 +99,7 @@ public class EvilBot : IChessBot
         //    }
         //}
 
-        Console.WriteLine("total piecevalue is:" + totalPieceValue);
+        //Console.WriteLine("total piecevalue is:" + totalPieceValue);
         
         return totalPieceValue;
     }
@@ -107,7 +107,7 @@ public class EvilBot : IChessBot
     {
         int pieceTypeIndex = (int)pieceType - 1;
 
-        Console.WriteLine(((x > 3 ? 7 - x : x /* this mirrors the table*/) + y * 4 + pieceTypeIndex * 32) * 2);
+        //Console.WriteLine(((x > 3 ? 7 - x : x /* this mirrors the table*/) + y * 4 + pieceTypeIndex * 32) * 2);
         return pieceValues[pieceTypeIndex] + (pieceTypeIndex == 0 ? y * 10 : int.Parse(pieceSqareValues.Substring(((x > 3 ? 7 - x : x /* this mirrors the table*/) + y * 4 + pieceTypeIndex * 32) * 2, 1)) * 5 - 50);
 
     }
